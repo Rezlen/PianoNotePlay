c70ae4be16dcb5518e2fc4d6218920daa23a0397
Rezlen <soheilyreza@yahoo.com>
2022-06-17 15:58:27 +0100

    Project completed

--- ProjectsCodeCademy/JavaScript/Create a Playlist App With the Spotify API/jamming/src/Components/App/App.js
+++ ProjectsCodeCademy/JavaScript/Create a Playlist App With the Spotify API/jamming/src/Components/App/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+
 
 /*When a user requests data from Spotify, the JSON response will include a set of song tracks. 
 Each track will contain a field for name, artist, and album. For each track in the results list, your Jammming web app will display the song name, artist, and album.
@@ -16,13 +18,17 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{name: 'name1', artist:'artist1', album:'album1', id: 1}, {name: 'name2', artist:'artist2', album:'album2', id: 2}, {name: 'name3', artist:'artist3', album:'album3', id: 3}],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [{name: 'PlayListName1', artist:'PlayListArtist1', album:'PlayListAlbum1', id: 4}, {name: 'PlayListName2', artist:'PlayListArtist2', album:'PlayListAlbum2', id: 5}, {name: 'PlayListName3', artist:'PlayListArtist3', album:'PlayListAlbum3', id: 6}]
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
+
   } 
 
 // if the track has not been added, then it aves it to the playlist
@@ -46,20 +52,37 @@ class App extends React.Component {
     this.setState({playlistName: name});
 
   }
+// Potential problem; the website instruction URIs are capital but the video instruction is in small letters
+  savePlaylist(){
+    const trackURIs =  this.state.playlistTracks.map(track=>track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
+  }
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    });
+  }
 
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar/>
+          <SearchBar  onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults searchResults = {this.state.searchResults} 
             onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks} 
             onRemove={this.removeTrack} 
-            onNameChange={this.updatePlaylistName}  />
+            onNameChange={this.updatePlaylistName} 
+            onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
---
+++ ProjectsCodeCademy/JavaScript/Create a Playlist App With the Spotify API/jamming/src/util/Spotify.js
@@ -0,0 +1,82 @@
+const clientId = '7af13fe261434eec9f26f8d37112661f';
+const redirectURI = 'http://rez-surge-school.surge.sh';
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+    return accessToken;
+    }
+  // Check for access token match
+  const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+  const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+        // Clear the parameters from the URL, so the app doesn’t try grabbing the access token after it has expired. Then allowing us to grab a new access token when it expires.
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+        return accessToken;
+    } else {
+      // potential confusion in URL & URI
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
+    }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(Response => {
+      return Response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artist[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+    });
+  },
+  savePlaylist(name, trackURIs) {
+    if(!name || !trackURIs.length) {
+      return;
+    }
+    const accessToken = Spotify.getAccessToken();
+    const headers = {Authorization: `Bearer ${accessToken}`};
+    let userId;
+// an erro here: src\util\Spotify.js   Line 58:12:  'response' is not defined  no-undef   Line 58:23:  'response' is not defined  no-undef
+    return fetch(`https://api.spotify.com/v1/me`, {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
+      {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({name: name})
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({uris: trackURIs})
+        });
+      });
+    });
+  }
+}
+
+
+
+
+export default Spotify;
