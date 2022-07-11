eda0e7aeaf6f51db6d7a140ee1bb6594acd5d914
Rezlen <soheilyreza@yahoo.com>
2022-07-11 09:55:43 +0100

    Start Of Backend

---
+++ Code Example & Refrences/backEnd.js
@@ -0,0 +1,35 @@
+How servers connect to the Internet
+Clients use a URL to connect to a server across the Internet. The URL has four main components:
+
+A connection scheme, usually HTTPS (“Hypertext Transfer Protocol Secure”), browsers and web-servers use to talk with one another.
+A subdomain that specifies the particular server (usually organized by resource type) to be delivered to the client.
+A domain name that specifies the name of the organization(s) associated with the URL.
+A top-level domain that specifies the type of organization associated with the URL.
+For the URL https://www.codecademy.com, “https” is the scheme, “www” is the subdomain, “codecademy” is the domain name, and “com” is the top-level domain. In further detail, the following happens:
+
+The browser first communicates with the internet service provider (ISP).
+This further communicates with the domain name server to get the IP address of the server.
+The domain name server converts a domain name to an IP address.
+Using this IP address, the browser connects to the server.
+Once connected to the web server, our browser sends the request to the server and asks for particular files.
+When the browser has connected to the server at the correct IP address, the servers send all the requested HTML text for the web page to our browser.
+Server connecting to the internet to send the response to client
+
+Examples of server operating systems
+The operating system (OS) used in a given server is completely dependent on the server’s requirements. The commonly used server operating systems are:
+
+Windows: This server operating system is provided by the Microsoft Corporation. It allows users to store the files, play videos and music. It also supports multitasking, graphical user interface and virtual memory management.
+
+UNIX: It is one of the most popular operating system for client-server environment. It is a multi-user operating system. This operating system is widely used by websites to provide services on the internet.
+
+Linux: This operating system is open-source, multi-user, multi-process and gives a good real-time performance. It offers low cost for delivering services to the clients. This operating system chosen over others for servers for their security services.
+
+Types of servers
+There are different types of servers based on their uses. Some of the most common types of servers are as follows:
+
+A file server is used to store data files for multiple users. They provide users access to stored files and data and can allow faster retrieval and saving of data. These are used on private networks and provides a location for storage of computer files. It provides a central location to store files where multiple users can work with the same documents.
+A database server allows another computer to access the database and retrieve or upload data. This type of server typically has a large storage capacity.
+A web server delivers web pages requested by multiple client web browsers.
+Mail servers store and deliver email for clients through an email service platform. They are a form of digital ‘post office’ that sorts and stores emails.
+An application server provides a software environment with all the needed requirements. This server allows users access to bypass downloading the software.
+The proxy server communicates with the websites you are visiting on your behalf. It links the user with the rest of the internet. The browser connects to the proxy server, and the proxy server sends your request to the website and sends the website’s response back to you.
