173682627d9fcd56058dfadafe413968731aefad
Rezlen <soheilyreza@yahoo.com>
2022-07-18 18:29:26 +0100

    Merge Parameters finished

--- Code Example & Refrences/ROUTER-PARAMETERS.js
+++ Code Example & Refrences/ROUTER-PARAMETERS.js
@@ -15,3 +15,172 @@ In the above code we need to extract the request parameter :sorcererName from th
 
 ------------
 
+router.param()
+Express, luckily, is mindful of the pain-point of replicated parameter-matching code and has a method specifically for this issue. When a specific parameter is present in a route, we can write a function that will perform the necessary lookup and attach it to the req object in subsequent middleware that is run.
+
+app.param('spellId', (req, res, next, id) => {
+  let spellId = Number(id);
+    try {
+      const found = SpellBook.find((spell) => {
+        return spellId === spell.id;
+      })
+      if (found) {
+        req.spell = found;
+        next();
+      } else {
+        next(new Error('Your magic spell was not found in any of our tomes'));
+      };
+    } catch (err) {
+      next(err)
+    }
+});
+In the code above we intercept any request to a route handler with the :spellId parameter. Note that in the app.param function signature, 'spellId' does not have the leading :. The actual ID will be passed in as the fourth argument, id in this case, to the app.param callback function when a request arrives.
+
+We look up the spell in our SpellBook array using the .find() method. If SpellBook does not exist, or some other error is thrown in this process, we pass the error to the following middleware (hopefully we’ve written some error-handling middleware, or included some externally-sourced error-handling middleware). If the spell exists in SpellBook, the .find() method will store the spell in found, and we attach it as a property of the request object (so future routes can reference it via req.spell). If the requested spell does not exist, .find() will store undefined in found, and we let future middlewares know there was an error with the request by creating a new Error object and passing it to next().
+
+Note that inside an app.param callback, you should use the fourth argument as the parameter’s value, not a key from the req.params object.
+
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+const spiceRack = [
+  {
+    id: 1,
+    name: 'cardamom',
+    grams: 45,
+  },
+  {
+    id: 2,
+    name: 'pimento',
+    grams: 20,
+  },
+  {
+    id: 3,
+    name: 'cumin',
+    grams: 450,
+  },
+  {
+    id: 4,
+    name: 'sichuan peppercorns',
+    grams: 107,
+  }
+];
+
+let nextSpiceId = 5;
+
+app.use(bodyParser.json());
+
+// Add your code here:
+app.param('spiceId', (req, res, next, id) => {
+  const spiceId = Number(id);
+  const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
+  
+  if (spiceIndex !== -1){
+    req.spiceIndex = spiceIndex;
+    next();
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+
+app.get('/spices/', (req, res, next) => {
+  res.send(spiceRack);
+});
+
+app.post('/spices/', (req, res, next) => {
+  const newSpice = req.body.spice;
+  if (newSpice.name  && newSpice.grams) {
+    newSpice.id = nextSpiceId++;
+    spiceRack.push(newSpice);
+    res.send(newSpice);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.get('/spices/:spiceId', (req, res, next) => {
+  res.send(spiceRack[req.spiceIndex]);
+});
+
+app.put('/spices/:spiceId', (req, res, next) => {
+  spiceRack[req.spiceIndex] = req.body.spice;
+  res.send(spiceRack[req.spiceIndex]);
+});
+
+app.delete('/spices/:spiceId', (req, res, next) => {
+  spiceRack.splice(req.spiceIndex, 1);
+  res.status(204).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+---------------
+
+Merge Parameters
+Complexity is all around us. Buildings are made from bricks and many droplets of water make a cloud. When we want to create something complex in software, we model out our base components and use composition to create these relationships.
+
+When we’re building web endpoints, we might want to access some property of a complex object. In order to do this in Express, we can design a nested router. This would be a router that is invoked within another router. In order to pass parameters the parent router has access to, we pass a special configuration object when defining the router.
+
+const sorcererRouter = express.Router();
+const familiarRouter = express.Router({mergeParams: true});
+ 
+sorcererRouter.use('/:sorcererId/familiars', familiarRouter);
+ 
+sorcererRouter.get('/', (req, res, next) => {
+  res.status(200).send(Sorcerers);
+  next();
+});
+ 
+sorcererRouter.param('sorcererId', (req, res, next, id) => {
+  const sorcerer = getSorcererById(id);   
+  req.sorcerer = sorcerer;
+  next();
+});
+ 
+familiarRouter.get('/', (req, res, next) => {
+  res.status(200).send(`Sorcerer ${req.sorcerer} has familiars ${getFamiliars(sorcerer)}`);
+});
+ 
+app.use('/sorcerer', sorcererRouter);
+In the code above we define two endpoints: /sorcerer and /sorcerer/:sorcererId/familiars. The familiars are nested into the sorcerer endpoint — indicating the relationship that a sorcerer has multiple familiars. Take careful note of the {mergeParameters: true} argument that gets passed when creating the familiarRouter. This argument tells Express that the familiarRouter should have access to parents passed into its parent router, that is, the sorcererRouter. We then tell express that the path for the familiarRouter is the same as the path for the sorcererRouter with the additional path /:sorcererId/familiars. We then can create a family of routes (a router) built by appending routes to familiarRouter‘s base: /sorcerer/:sorcererId/familiars.
+
+---------------
+
+Merge Parameters
+Complexity is all around us. Buildings are made from bricks and many droplets of water make a cloud. When we want to create something complex in software, we model out our base components and use composition to create these relationships.
+
+When we’re building web endpoints, we might want to access some property of a complex object. In order to do this in Express, we can design a nested router. This would be a router that is invoked within another router. In order to pass parameters the parent router has access to, we pass a special configuration object when defining the router.
+
+const sorcererRouter = express.Router();
+const familiarRouter = express.Router({mergeParams: true});
+ 
+sorcererRouter.use('/:sorcererId/familiars', familiarRouter);
+ 
+sorcererRouter.get('/', (req, res, next) => {
+  res.status(200).send(Sorcerers);
+  next();
+});
+ 
+sorcererRouter.param('sorcererId', (req, res, next, id) => {
+  const sorcerer = getSorcererById(id);   
+  req.sorcerer = sorcerer;
+  next();
+});
+ 
+familiarRouter.get('/', (req, res, next) => {
+  res.status(200).send(`Sorcerer ${req.sorcerer} has familiars ${getFamiliars(sorcerer)}`);
+});
+ 
+app.use('/sorcerer', sorcererRouter);
+In the code above we define two endpoints: /sorcerer and /sorcerer/:sorcererId/familiars. The familiars are nested into the sorcerer endpoint — indicating the relationship that a sorcerer has multiple familiars. Take careful note of the {mergeParameters: true} argument that gets passed when creating the familiarRouter. This argument tells Express that the familiarRouter should have access to parents passed into its parent router, that is, the sorcererRouter. We then tell express that the path for the familiarRouter is the same as the path for the sorcererRouter with the additional path /:sorcererId/familiars. We then can create a family of routes (a router) built by appending routes to familiarRouter‘s base: /sorcerer/:sorcererId/familiars.
+
+--------------
+
