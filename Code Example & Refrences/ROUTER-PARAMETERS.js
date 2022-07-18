5f0390f47c3ed7bfcb8911f414cd3ac488ae9780
Rezlen <soheilyreza@yahoo.com>
2022-07-18 18:46:00 +0100

    cors

--- Code Example & Refrences/ROUTER-PARAMETERS.js
+++ Code Example & Refrences/ROUTER-PARAMETERS.js
@@ -182,5 +182,163 @@ familiarRouter.get('/', (req, res, next) => {
 app.use('/sorcerer', sorcererRouter);
 In the code above we define two endpoints: /sorcerer and /sorcerer/:sorcererId/familiars. The familiars are nested into the sorcerer endpoint — indicating the relationship that a sorcerer has multiple familiars. Take careful note of the {mergeParameters: true} argument that gets passed when creating the familiarRouter. This argument tells Express that the familiarRouter should have access to parents passed into its parent router, that is, the sorcererRouter. We then tell express that the path for the familiarRouter is the same as the path for the sorcererRouter with the additional path /:sorcererId/familiars. We then can create a family of routes (a router) built by appending routes to familiarRouter‘s base: /sorcerer/:sorcererId/familiars.
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+app.use(bodyParser.json());
+
+const spiceRacks = [
+  {
+    id: 1,
+    location: 'Kitchen Countertop',
+  },
+  {
+    id: 2,
+    location: 'Pantry',
+  },
+  {
+    id: 3,
+    location: 'Outdoor Barbecue',
+  }
+];
+
+let nextSpiceRackId = 4;
+
+app.param('spiceRackId', (req, res, next, id) => {
+  const idToFind = Number(id);
+  const rackIndex = spiceRacks.findIndex(spiceRack => spiceRack.id === idToFind);
+  if (rackIndex !== -1) {
+    req.spiceRack = spiceRacks[rackIndex];
+    req.spiceRackIndex = rackIndex;
+    next();
+  } else {
+    res.status(404).send('Spice Rack Not Found.');
+  }
+});
+
+app.get('/spice-racks/', (req, res, next) => {
+ res.send(spiceRacks);
+});
+
+app.post('/spice-racks/', (req, res, next) => {
+  const newRack = req.body.spiceRacks;
+  newRack.id = nextSpiceRackId++;
+  spiceRacks.push(newRack);
+  res.send(newRack);
+});
+
+app.get('/spice-racks/:spiceRackId', (req, res, next) => {
+  res.send(req.spiceRack);
+});
+
+app.put('/spice-racks/:spiceRackId', (req, res, next) => {
+  const updatedRack = req.body.spiceRack;
+  if (req.spiceRack.id !== updatedRack.id) {
+    res.status(400).send('Cannot update Spice Rack Id');
+  } else {
+    spiceRacks[req.spiceRackIndex] = updatedRack;
+    res.send(spiceRacks[req.spiceRackIndex]);
+  }
+});
+
+app.delete('/spice-racks/:spiceRackId', (req, res, next) => {
+  spiceRacks.splice(req.spiceRackIndex, 1);
+  res.status(204).send();
+});
+
+const spicesRouter = require('./spicesRouter');
+
+// Write your code below:
+app.use('/spice-racks/:spiceRackId/spices', spicesRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 --------------
+
+Review
+router.param is a powerful tool that we can use to keep our code from repeating core functionality through routes. This is a pattern we want to frequently follow: identify multiple pieces of code that accomplish the same goal, put it into a single component, let that component do that thing (and update it when we want the thing it does to change — in a single place).
+
+Let’s try applying that knowledge again, to another codebase. If you look at the workspace you’ll find the same problem of data-lookup happening, based on a URL parameter, multiple times in different places. Try combining that logic in a single place using router.param.
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+const vendingMachine = [
+  {
+    id: 1,
+    name: 'Gum',
+    price: 1.25,
+  },
+  {
+    id: 7,
+    name: 'Bag of chips',
+    price: 3.50,
+  },
+  {
+    id: 23,
+    name: 'cumin',
+    price: .75,
+  }
+];
+
+let nextSnackId = 24;
+
+app.use(bodyParser.json());
+
+// Add your code here:
+app.param('snackId', (req, res, next, id) => {
+  const snackId = Number(id);
+  const snackIndex = vendingMachine.findIndex(snack => snack.id === snackId);
+  if (snackIndex === -1) {
+    res.status(404).send('Snack not found!');
+  } else {
+    req.snackIndex = snackIndex;
+    next();
+  }
+});
+
+app.get('/snacks/', (req, res, next) => {
+  res.send(vendingMachine);
+});
+
+app.post('/snacks/', (req, res, next) => {
+  const newSnack = req.body;
+  if (!newSnack.name || !newSnack.price) {
+    res.status(400).send('Snack not found!');
+  } else {
+    newSnack.id = nextSnackId++;
+    vendingMachine.push(newSnack);
+    res.send(newSnack);
+  }
+});
+
+app.get('/snacks/:snackId', (req, res, next) => {
+  res.send(vendingMachine[req.snackIndex]);
+});
+
+app.put('/snacks/:snackId', (req, res, next) => {
+  vendingMachine[req.snackIndex] = req.body;
+  res.send(vendingMachine[req.snackIndex]);
+});
+
+app.delete('/snacks/:snackId', (req, res, next) => {
+  vendingMachine.splice(req.snackIndex, 1);
+  res.status(204).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+----------------
 
