989fb83c066799386a8e70178dd0e58b2cfb07be
Rezlen <soheilyreza@yahoo.com>
2022-08-04 09:59:49 +0100

    Amazone clone project instaled

---
+++ ProjectsCodeCademy/amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <h1>Logo name come here </h1>
+      {/*Header*/}
+      {/**/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ProjectsCodeCademy/amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhIDKI8bNlGaM8FTd2KgBbewMEzhgl6u0",
+  authDomain: "amazo-clone1.firebaseapp.com",
+  projectId: "amazo-clone1",
+  storageBucket: "amazo-clone1.appspot.com",
+  messagingSenderId: "706401718927",
+  appId: "1:706401718927:web:d7410a4d107835621ed6fa",
+  measurementId: "G-BYV91BM3P8"
+};
