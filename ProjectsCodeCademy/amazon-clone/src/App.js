887bbc67d3ce56a5114978ca9949c48ae29b036e
Rezlen <soheilyreza@yahoo.com>
2022-08-05 18:09:41 +0100

    Compiled with problems:

--- ProjectsCodeCademy/amazon-clone/src/App.js
+++ ProjectsCodeCademy/amazon-clone/src/App.js
@@ -1,18 +1,29 @@
-// import logo from http://pngimg.com/uploads/amazon/amazon_PNG11.png;
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 
 function App() {
   return (
-    //BEM
-    <div className="App">
-      <Header />
-      <Home />
-
-
-    </div>
+    <Router>
+      {/*//BEM*/}
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+              <Checkout />
+          </Route>
+          {/*This default route must always be at the bottom otherwise will never be read.*/}
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- ProjectsCodeCademy/amazon-clone/src/Product.js
+++ ProjectsCodeCademy/amazon-clone/src/Product.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { startTransition } from 'react'
 import './Product.css';
 
 
 // "Product" is the CARDS/ITEMS inside page
-function Product() {
+function Product({id, title, image, price, rating}) {
   return (
     <div className='product'>
 
       <div className='product__info'> 
-      <p>The learn Info</p>
-      <p className='product__price' >
-        <small>$</small>
-        <strong>19.99</strong>
-      </p>
-      <div className='product__rating' >
-        <p>fohof :sta :</p>
-        <p></p>
-        <p></p>
+        <p>{title}</p>
+        <p className='product__price' >
+          <small>£</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='product__rating' >
+          {Array(rating) .fill() .map((_, i) => (<p>🌟</p>))}
+        </div>
       </div>
 
-      </div>
+      <img 
+      src={image}
+      alt='Amazon product image fom: https://images-na.ssl-images-amazon.com/images/I/610hYytBgdL.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+      />
+
+      <button>Add To Basket</button>
     </div>
   );
 }
