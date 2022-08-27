4054a8bad71b897f0e88bb8c3b36952662bba30d
Rezlen <soheilyreza@yahoo.com>
2022-08-27 23:06:26 +0100

    Error in Chatpage

--- ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/frontend/src/App.js
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import { Button } from "@chakra-ui/react"; // provides some UI components.
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
+//import { Button } from "@chakra-ui/react"; // provides some UI components.
 
 
 function App() {
   return (
     <div className="App">
       Hello world backend is on port 5000 and front on 3000.
-      <Button colorScheme='pink' >Button</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/chats" element={<ChatPage />} />
+        {/* <Route path='/login' element={<Login />} />
+          <Route path='/orders' element={[<Header />, <Orders />]} />
+          <Route path='/checkout' element={[<Header />, <Checkout />]} />
+          <Route path='/payment' element={[<Header />, <Elements stripe={stripePromise} > <Payment/> </Elements>]} /> {/*whole component PAYMENT you're showing here should be wrapped by Elements, not some part of. <Elements stripe={stripePromise} > <Payment/> </Elements> */}
+        {/* <Route path="/" element={[<Header />, <Home />]} /> */}
+      </Routes>
+      {/* <Button colorScheme="pink">Button</Button> */}
     </div>
   );
 }
