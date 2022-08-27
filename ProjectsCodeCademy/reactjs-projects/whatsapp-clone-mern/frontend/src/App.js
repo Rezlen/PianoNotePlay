748330b26336a60eff1c3dde324bfa92d06b1af8
Rezlen <soheilyreza@yahoo.com>
2022-08-27 21:06:32 +0100

    React, Dom, Chackra, Express Backend.. Insalled,

---
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from "@chakra-ui/react"; // provides some UI components.
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello world backend is on port 5000 and front on 3000.
+      <Button colorScheme='pink' >Button</Button>
+    </div>
+  );
+}
+
+export default App;
