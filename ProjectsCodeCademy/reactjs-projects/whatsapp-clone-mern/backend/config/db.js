71f4d0e40a97dafc5d513112449090862080b143
Rezlen <soheilyreza@yahoo.com>
2022-09-20 23:08:38 +0100

    MongoDB connected

--- ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/config/db.js
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose"); // this get imported from "package.json" file, in the root folder
-const colors = require("colors");
+//const colors = require("colors");
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, { // this will be responsible to connect ot the DB. This is the connection string (Key pass/URI), which is stored in the ".env" file, in the root folder
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: true,
+      // useFindAndModify: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.bold);
+    console.log(`Error: ${error.message}`);
     process.exit();
   }
 };
