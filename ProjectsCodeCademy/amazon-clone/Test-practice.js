67ca30425e941fd443556631bbc90855ff8b90f4
Rezlen <soheilyreza@yahoo.com>
2022-09-29 20:26:57 +0100

    Odd Even

--- ProjectsCodeCademy/amazon-clone/Test-practice.js
+++ ProjectsCodeCademy/amazon-clone/Test-practice.js
@@ -2,14 +2,28 @@
 
 var my_list = [1, 2, 3, 4];
 
-const find_total = (my_list) => {
+function find_total(my_list) {
   let total = 0;
   for (let i = 0; i < my_list.length; i++) {
     total += my_list[i];
   }
   return total;
 };
+
 console.log(find_total(my_list));
 
 
-console.log([1,2,3,4].reduce((a,b) => a + b, 0));
+console.log([1, 2, 3, 4].reduce((a, b) => a + b, 0));
+------------------
+function isEven(n) {
+   return n % 2 == 0;
+}
+
+function isOdd(n) {
+   return Math.abs(n % 2) == 1;
+}
+
+for(let count =0; count<=100;count++){
+ count%2==0? console.log(`${count} is even`):console.log(`${count} is odd`);
+ ;
+}
