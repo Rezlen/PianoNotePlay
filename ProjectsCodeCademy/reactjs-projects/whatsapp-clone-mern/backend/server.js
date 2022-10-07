cd2f9771c629a8c067dc7f26cf9e48332ab0bbd9
Rezlen <soheilyreza@yahoo.com>
2022-10-07 09:44:34 +0100

    Small Changes

--- ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/server.js
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
+// const { chats } = require("./data/data");
 const colors = require("colors"); // this is just have colour in your terminal
 const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
