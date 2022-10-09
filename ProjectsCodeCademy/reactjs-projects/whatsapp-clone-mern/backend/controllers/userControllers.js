e6ed3a22a4f986a02fd7d81b295e4692d1dee820
Rezlen <soheilyreza@yahoo.com>
2022-10-09 11:51:16 +0100

    Route.get() requires a callback function but got a [object Undefined]. Solved

--- ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/controllers/userControllers.js
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/controllers/userControllers.js
@@ -2,6 +2,26 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+//@description     Register new user
+//@route           POST /api/user/
+//@access          Public
 
 const registerUser = asyncHandler(async (req, res) => {
 
@@ -51,4 +71,4 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+module.exports = { allUsers, registerUser, authUser };
--- ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/routes/userRoutes.js
+++ ProjectsCodeCademy/reactjs-projects/whatsapp-clone-mern/backend/routes/userRoutes.js
@@ -1,12 +1,16 @@
 // connection between the routes and the DB/users.
 const express = require("express");
-const { registerUser, authUser, allUsers } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // these are controllers which must have their own folders & files
-router.route("/").get(protect, allUsers);
+router.route("/").get(protect, );
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
