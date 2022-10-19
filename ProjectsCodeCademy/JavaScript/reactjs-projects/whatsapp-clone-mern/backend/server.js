065051048fd4cea3e4b120f803960ecbaecb2cd1
Rezlen <soheilyreza@yahoo.com>
2022-10-19 09:42:20 +0100

    GitIgnore added

--- ProjectsCodeCademy/JavaScript/reactjs-projects/whatsapp-clone-mern/backend/server.js
+++ ProjectsCodeCademy/JavaScript/reactjs-projects/whatsapp-clone-mern/backend/server.js
@@ -15,22 +15,40 @@ const app = express();
 
 app.use(express.json()); // since we are getting user data from frontend, we must tell the json file to accept it. 
 
-app.get('/', (req, res) => {
-  res.send("API is running successfully");
-  app.use("/api/user", userRoutes);
-});
+// app.get('/', (req, res) => {
+//   res.send("API is running successfully");
+//   app.use("/api/user", userRoutes);
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// error handling middle ware
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-// This requests all of the chats & display here: http://localhost:5000/api/chat
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
+// // This requests all of the chats & display here: http://localhost:5000/api/chat
+// app.get('/api/chat', (req, res) => {
+//   res.send(chats);
 
   const PORT = process.env.PORT;
 
@@ -79,13 +97,13 @@ app.get('/api/chat', (req, res) => {
   });
 });
 
-// This requests a specific chat and display here: http://localhost:5000/api/chat/:id
-app.get('/api/chat/:id', (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id); // 'C' stands for COMPARE.
-  res.send(singleChat);
-});
+// // This requests a specific chat and display here: http://localhost:5000/api/chat/:id
+// app.get('/api/chat/:id', (req, res) => {
+//   // console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.id); // 'C' stands for COMPARE.
+//   res.send(singleChat);
+// });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server started on PORT ${PORT}` .bold));
+// app.listen(5000, console.log(`Server started on PORT ${PORT}` .bold));
